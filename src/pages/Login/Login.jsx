150109cad1e24527ccc2b50dea1ff2f67fc8d0b2
saedpanah <m.saedpanah76@gmail.com>
2023-11-04 12:36:29 +0330

    changes21

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
+import './Login.css'
+
 const Login = () => {
   return (
-    <div>
-      
+    <div className='login-container'>
+      <div className='login-wrapper'>
+        <h1 className='login-title'>SIGN IN</h1>
+        <form className='login-form'>
+          <input placeholder='username' className='login-input'/>
+          <input placeholder='password' className='login-input'/>
+          <button className='login-button'> LOGIN </button>
+          <Link to='/register' style={{textDecoration:'none', color :'teal', fontSize:'14px' , fontWeight:'bold'}}> CREATE A NEW ACCOUNT</Link>
+        </form>
+      </div>
     </div>
   )
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
+import './Register.css'
 const Register = () => {
   return (
-    <div>
-      
+    <div className='register-container'>
+      <div className='register-wrapper'>
+        <h1 className='register-title'></h1>
+          <form className='register-form'>
+            <input className='register-input' placeholder='name'/>
+            <input className='register-input' placeholder='last name'/>
+            <input className='register-input' placeholder='username'/>
+            <input className='register-input' placeholder='email'/>
+            <input className='register-input' placeholder='password'/>
+            <input className='register-input' placeholder='confirm password'/>
+            <span className='agreement'>
+                By creating an account, I consent to the processing of my personal
+                data in accordance with the <b>PRIVACY POLICY</b>
+            </span>
+            <button className='register-button'>
+              CREATE
+            </button>
+          </form>
+      </div>
     </div>
   )
 }
